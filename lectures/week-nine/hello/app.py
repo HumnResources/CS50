9d6f9fb5064589e80a6f4447df72a9cb576c8428
Chase Z. Laptop <ziskachase@gmail.com>
2021-06-23 18:12:22 -0400

    More notes for week nine lecture. Worked on some example code w/ flask

--- lectures/week-nine/hello/app.py
+++ lectures/week-nine/hello/app.py
@@ -5,8 +5,11 @@ app = Flask(__name__) # refers to the name of the current file
 
 @app.route("/")
 def index():
-    return render_template("index.html", name=request.args.get("name", "world")) # Parses the 'name' http variable
+    # <param passed to template> = request.args.get(<http param>, <default>) 
+    return render_template("/index.html") 
 
-@app.route("/greet")
+# Defaults to GET, need to specify for a POST etc
+@app.route("/greet", methods=["POST"])
 def greet():
-    return "TODO" # render_template("index.html", name=request.args.get("name", "world")) # Parses the 'name' http variable
+    return render_template("/greet.html", name=request.form.get("name", "world")) # Parses the 'name' http variable
+
